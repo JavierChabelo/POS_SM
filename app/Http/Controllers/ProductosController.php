ccd63dc8b6c8406e56216e4d88365c1f6890c000
JavierChabelo <reniorrulez@gmail.com>
2024-12-01 12:31:52 -0600

    Se agrega validaciopn cuando codigo de barras ya existe

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -37,8 +37,26 @@ class ProductosController extends Controller
      */
     public function store(Request $request)
     {
+        // Verificar si el código de barras existe
+        $barCode = Producto::where('codigo_barras', $request->codigo_barras)->first();
+
+        if ($barCode !== null) {
+            // Si el código de barras ya existe, redirigir con un mensaje de error
+            return redirect()->route("productos.index")->with("mensaje", "Este código de barras ya existe");
+        }
+
+        // Verificar si el ID ya existe
+        $existingProduct = Producto::where('id', $request->id)->first();
+
+        if ($existingProduct !== null) {
+            // Si el ID ya existe, redirigir con un mensaje de error
+            return redirect()->route("productos.index")->with("mensaje", "Este ID ya existe");
+        }
+
+        // Si el código de barras y el ID no existen, crear un nuevo producto
         $producto = new Producto($request->input());
         $producto->saveOrFail();
+
         return redirect()->route("productos.index")->with("mensaje", "Producto guardado");
     }
 
